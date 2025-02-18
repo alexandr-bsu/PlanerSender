4f7794468e7acfae8cf7bac67db6d2dd7afae88b
Alexandr Rodionov <rodionov.alexandr-sergeevich@yandex.ru>
2025-02-18 16:28:34 +0300

    update

--- src/routers/sender.py
+++ src/routers/sender.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, BackgroundTasks
-from services.sender import SenderService
+from services.sender import SenderService, resolve_username
 import asyncio
 
 sender_router = APIRouter(prefix='/sender')
@@ -8,8 +8,13 @@ def run_sender():
     sender = SenderService()
     asyncio.run(sender.run())
 
+@sender_router.get('/login')
+async def login():
+    await resolve_username('none_user_404')
+    return {'status': 'ok'}
+
 @sender_router.get('/')
-async def get_orders_report(background_tasks: BackgroundTasks):
+async def run_sender(background_tasks: BackgroundTasks):
     background_tasks.add_task(run_sender)
     return {"status": "ok"}
 
