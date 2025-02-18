ba2f37ee1d6c8e59cfe03189da407acfe557376c
Alexandr Rodionov <rodionov.alexandr-sergeevich@yandex.ru>
2025-02-18 16:43:30 +0300

    update

--- docker/app_test.sh
+++ docker/app_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd src
-gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8001
+gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8001
